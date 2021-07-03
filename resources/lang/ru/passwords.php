d452016eef5192d23af4979da646ec50c662910d
artem <artemmanvelyan1@gmail.com>
2021-07-03 19:34:05 +0900

    init

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Пароль был сброшен!',
+    'sent' => 'Ссылка для сброса пароля отправлена на вашу электронную почту!',
+    'throttled' => 'Пожалуйста, подождите, прежде чем повторить попытку.',
+    'token' => 'Токен сброса пароля недействителен.',
+    'user' => "Пользователь с таким адресом электронной почты не найден",
+
+];
